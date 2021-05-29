1a8dbe60932a75e213ac23a9ce86613d3527fa93
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-29 15:44:40 +0200

    Create a function handleMode and move this to util.js file

--- src/js/main.js
+++ src/js/main.js
@@ -20,30 +20,8 @@ const url = `https://api.github.com/users/`;
 // Manage body's loading
 window.addEventListener("load", () => document.body.classList.remove("loading"));
 
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
-  document.body.classList.toggle("dark-theme");
-} else if (currentTheme == "light") {
-  document.body.classList.toggle("light-theme");
-}
-
-$toogleMode.addEventListener("click", function () {
-    let theme;
-    if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("light-theme");
-    theme = document.body.classList.contains("light-theme")
-      ? "light"
-      : "dark";
-  } else {
-    document.body.classList.toggle("dark-theme");
-    theme = document.body.classList.contains("dark-theme")
-      ? "dark"
-      : "light";
-  }
-  localStorage.setItem("theme", theme);
-});
+// Switch mode's theme between dark and light
+util.handleMode(window, localStorage, document.body, $toogleMode);
 
 // Listening user's input
 $input.addEventListener("input", () =>{
